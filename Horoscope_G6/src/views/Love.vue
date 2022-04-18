a46cc91a562eb1d2fccde22c220e528e7d57cd7f
wareewan-opal <68785361+wareewan-opal@users.noreply.github.com>
2022-04-18 16:17:21 +0700

    Finally Project backup file

---
+++ Horoscope_G6/src/views/Love.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {ref , onBeforeMount} from 'vue'
+import card_component from '../components/card_component.vue';
+const deck = ref([])
+const type = ref('love');
+//Get Card
+const getCard = async () => {
+  const res = await fetch ('http://localhost:5000/major-arcana')
+  if(res.status === 200){
+    deck.value = await res.json()
+  }
+}
+onBeforeMount(async () =>{
+  await getCard()
+})
+</script>
+ 
+<template>
+    <h1>LOVE HOROSCOPE</h1>
+    <h6>PLEASE SELECTE 3  CARDS</h6>
+    <card_component :deck="deck" :typeofcard="type"></card_component>
+</template>
+
+<style>
+h1{  
+  font-size: 2.3em;
+  font-family: 'Cinzel Decorative', cursive;
+  text-align: center;
+  padding-top: 1em;
+  }    
+</style>
